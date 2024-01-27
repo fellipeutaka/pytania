267b4dbc87ed34650bb5fb4d9d86b268be8b7dd5
Fellipe Utaka <fellipeutaka@gmail.com>
2024-01-27 04:04:40 -0300

    chore: remove prefetch on /quiz

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,16 +1,12 @@
 import Link from "next/link";
 
-import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
 import { Icons } from "~/components/icons";
 import { Button } from "~/components/ui/button";
-import { helpers } from "~/lib/api/server";
 import { QuizList } from "./quiz-list";
 
 export const revalidate = 30;
 
-export default async function Page() {
-  await helpers.quiz.findMany.prefetch();
-
+export default function Page() {
   return (
     <main className="container pb-12 pt-16 lg:py-28">
       <div className="mb-6 flex items-center justify-between motion-safe:animate-fade">
@@ -22,9 +18,7 @@ export default async function Page() {
           </Link>
         </Button>
       </div>
-      <HydrationBoundary state={dehydrate(helpers.queryClient)}>
-        <QuizList />
-      </HydrationBoundary>
+      <QuizList />
     </main>
   );
 }
